aaa84afe986fd91f00a3da164e2f7780af54458c
Dmitriy <altusj@gmail.com>
2023-12-08 18:42:15 -0500

    Add solution to arrays

--- assessments/module001/arrays.js
+++ assessments/module001/arrays.js
@@ -5,10 +5,14 @@ const faveColors = ['red', 'green', 'black']
 
 //CODE HERE
 
+let colorCopy = faveColors.slice(0, faveColors.length);
+
 //////////////////PROBLEM 2////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
 
 //CODE HERE
+
+colorCopy.push('white');
 
 //////////////////PROBLEM 3////////////////////
 /*
@@ -20,6 +24,7 @@ const numbers = [1, 2, 3, 4, 5]
 
 //CODE HERE
 
+const middleNums = numbers.slice(1, numbers.length - 1);
 
 //////////////////PROBLEM 4////////////////////
 /* 
