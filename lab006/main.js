d63b152259af19d5a3ffe2b677c2a68a2c66b2a4
Dmitriy <altusj@gmail.com>
2023-12-16 11:26:49 -0500

    add lab006

--- lab006/main.js
+++ lab006/main.js
@@ -1,3 +1,7 @@
 // Given a number, n, return an array containing n unique random numbers between 1-10, inclusive.
 // That is, do not repeat any numbers in the returned list.
 // You can trust that this function will never be called with n < 0 or n > 10.
+
+function luckyNumbers(count){
+    
+}
