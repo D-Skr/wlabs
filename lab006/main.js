44cdb3575498a068df0826bc6f5bfadb4872a2e8
Dmitriy <altusj@gmail.com>
2023-12-16 11:24:16 -0500

    add lab006

---
+++ lab006/main.js
@@ -0,0 +1,3 @@
+// Given a number, n, return an array containing n unique random numbers between 1-10, inclusive.
+// That is, do not repeat any numbers in the returned list.
+// You can trust that this function will never be called with n < 0 or n > 10.
