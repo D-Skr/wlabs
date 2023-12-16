0b5465770efb95e78eea590425d6b95f7cffccc8
Dmitriy <altusj@gmail.com>
2023-12-16 11:56:27 -0500

    add isPalindrome solution

--- lab006/isPalindrome.js
+++ lab006/isPalindrome.js
@@ -2,3 +2,12 @@
 // A palindrome is a word that is spelled the same backwards and forwards.
 // Case sensitive.
 
+function isPalindrome(word) {
+    for (let i = 0; i < word.length; i++) {
+        //if (word.charCodeAt(i) != word.charCodeAt(word.length - 1 - i)) {
+        if (word[i] != word[word.length - 1 - i]) {
+            return false;
+        }
+        return true;
+    }
+}
