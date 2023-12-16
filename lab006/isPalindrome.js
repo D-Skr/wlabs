e4466d75d5bc5b512a2b909ceae4b20fc2d990a8
Dmitriy <altusj@gmail.com>
2023-12-16 12:02:04 -0500

    refactor

--- lab006/isPalindrome.js
+++ lab006/isPalindrome.js
@@ -3,7 +3,7 @@
 // Case sensitive.
 
 function isPalindrome(word) {
-    for (let i = 0; i < word.length; i++) {
+    for (let i = 0; i < word.length / 2; i++) {
         //if (word.charCodeAt(i) != word.charCodeAt(word.length - 1 - i)) {
         if (word[i] != word[word.length - 1 - i]) {
             return false;
