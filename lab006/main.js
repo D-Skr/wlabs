7a1e0451f9d2b03c404472e9526eb23ddc26102c
Dmitriy <altusj@gmail.com>
2023-12-16 11:36:18 -0500

    add lab006 solution

--- lab006/main.js
+++ lab006/main.js
@@ -2,6 +2,15 @@
 // That is, do not repeat any numbers in the returned list.
 // You can trust that this function will never be called with n < 0 or n > 10.
 
-function luckyNumbers(count){
-    
+function luckyNumbers(count) {
+    const numbersRange = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    let result = []; //
+
+    for (i = 0; i < count; i++) {
+        let index = Math.floor(Math.random() * numbersRange.length);
+        result.push(numbersRange[index]);
+        numbersRange.splice(index, 1);
+    }
+    return result;
 }
