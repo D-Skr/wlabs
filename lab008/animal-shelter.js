8b42b026a2aa169359ba080387b30aabbac80df4
Dmitriy <altusj@gmail.com>
2023-12-19 21:12:34 -0500

    add solution to animal shelter

--- lab008/animal-shelter.js
+++ lab008/animal-shelter.js
@@ -1,1 +1,28 @@
 const animalData = require('./animal-data.json');
+
+class Animal {
+    constructor(name, species, color, hunger) {
+        this.name = name;
+        this.species = species;
+        this.color = color;
+        this.hunger = hunger;
+    }
+
+    greet() {
+        return `Hi, ${this.name} ${this.species}`
+    }
+
+    feed() {
+        this.hunger -= 20;
+        return `Yum, I love food`
+    }
+
+
+}
+
+const dog = new Animal('Dog1', 'Dog', 'black', 100);
+
+console.log(dog.greet());
+console.log(dog.hunger);
+console.log(dog.feed());
+console.log(dog.hunger);
