f6274e52b33d80d2502d4c77902ac7830cde2f30
Dmitriy <altusj@gmail.com>
2024-01-06 17:19:01 -0500

    add lab013 Movie List delete function

--- lab013/index.js
+++ lab013/index.js
@@ -7,9 +7,18 @@ function addMovie(evt) {
     const movieTitle = document.createElement('span');
     movieTitle.textContent = inputField.value;
     movie.appendChild(movieTitle);
+    const deleteBtn = document.createElement('button');
+
+    deleteBtn.textContent = 'X';
+    deleteBtn.addEventListener('click', deleteMovie);
+    movie.appendChild(deleteBtn);
+
     document.querySelector('ul').appendChild(movie);
     inputField.value = '';
 }
 
-
 form.addEventListener('submit', addMovie);
+
+function deleteMovie(evt) {
+    evt.target.parentNode.remove();
+}
