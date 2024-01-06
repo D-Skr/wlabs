503cdab11718a14d9a60911402b92674f2453640
Dmitriy <altusj@gmail.com>
2024-01-06 11:34:09 -0500

    add solution

--- lab012/stockPrices.js
+++ lab012/stockPrices.js
@@ -14,3 +14,26 @@ function best(array) {
 
 let arr = [10, 2, 3, 32, 5]; //30
 console.log(best(arr));
+
+const best2 = prices => {
+    let maxDiff = 0
+    let lowSoFar
+
+    for (let i = 0; i < prices.length; i++) {
+        if (lowSoFar === undefined || prices[i] < lowSoFar) {
+            lowSoFar = prices[i]
+        }
+
+        let diff = prices[i] - lowSoFar
+
+        if (diff > maxDiff) {
+            maxDiff = diff
+        }
+    }
+
+    return maxDiff
+}
+
+// best([1, 2, 3, 4, 5])\
+// best([2, 3, 10, 6, 4, 8, 1])
+// best([5, 4 , 3, 2, 1])
