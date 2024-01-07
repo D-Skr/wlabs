1845f4b44f3219d8369cc4bcf0fd5be01b81670e
Dmitriy <altusj@gmail.com>
2024-01-06 21:17:07 -0500

    add message function

--- lab013/index.js
+++ lab013/index.js
@@ -1,4 +1,5 @@
 const form = document.querySelector('form');
+const message = document.querySelector('#message');
 
 function addMovie(evt) {
     evt.preventDefault();
@@ -23,8 +24,18 @@ form.addEventListener('submit', addMovie);
 
 function deleteMovie(evt) {
     evt.target.parentNode.remove();
+    message.textContent = 'Movie deleted!';
+    revealMessage();
 }
 
 function crossOfMovie(evt) {
     evt.target.classList.toggle('checked');
+    if (evt.target.classList.contains('checked')) message.textContent = 'Movie watched!';
+    else message.textContent = 'Movie added back';
+    revealMessage();
+}
+
+function revealMessage() {
+    message.classList.remove('hide');
+    setTimeout(() => { message.classList.add('hide') }, 1500)
 }
