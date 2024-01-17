96dc3421af7ebb8df501a7af96e1f61ba627c8f4
Dmitriy <altusj@gmail.com>
2024-01-16 20:58:17 -0500

    add app.get wheather endpoint

--- lab015/server/index.js
+++ lab015/server/index.js
@@ -12,4 +12,10 @@ app.get("/api/users", (req, res) => {
   res.status(200).send(friends);
 });
 
+app.get("wheather/:temperature", (req, res) => {
+  const { temperature } = req.params;
+  const phrase = `<h3>It was ${temperature} today</h3>`;
+  res.status(200).send(phrase);
+});
+
 app.listen(4000, () => console.log("Server runs on 4000 port"));
