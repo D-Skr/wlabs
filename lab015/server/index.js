d3831f09501ac2766afb4c84d72b96647c5e7c10
Dmitriy <altusj@gmail.com>
2024-01-16 20:51:30 -0500

    add cors dependency

--- lab015/server/index.js
+++ lab015/server/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
 // accept json
 app.use(express.json());
+
+app.get("/api/users", (req, res) => {});
 
 app.listen(4000, () => console.log("Server runs on 4000 port"));
