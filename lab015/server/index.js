fd566f45392db9aaaf5fa73efc7a7dc7bf56c14f
Dmitriy <altusj@gmail.com>
2024-01-16 20:55:24 -0500

    add app.get

--- lab015/server/index.js
+++ lab015/server/index.js
@@ -3,9 +3,13 @@ const cors = require("cors");
 
 const app = express();
 
-// accept json
+// accept json and cors
 app.use(express.json());
+app.use(express.cors());
 
-app.get("/api/users", (req, res) => {});
+app.get("/api/users", (req, res) => {
+  let friends = ["Bob", "John", "Sam", "Smith", "Billy"];
+  res.status(200).send(friends);
+});
 
 app.listen(4000, () => console.log("Server runs on 4000 port"));
