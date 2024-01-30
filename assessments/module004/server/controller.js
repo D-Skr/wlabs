3210cb903e42f4529a6a6068f1c9eb5d4b7c7e2b
Dmitriy <altusj@gmail.com>
2024-01-30 18:32:29 -0500

    add solution

--- assessments/module004/server/controller.js
+++ assessments/module004/server/controller.js
@@ -1,13 +1,31 @@
 module.exports = {
+  getCompliment: (req, res) => {
+    const compliments = [
+      "Gee, you're a smart cookie!",
+      "Cool shirt!",
+      "Your Javascript skills are stellar.",
+    ];
 
-    getCompliment: (req, res) => {
-        const compliments = ["Gee, you're a smart cookie!", "Cool shirt!", "Your Javascript skills are stellar."];
-      
-        // choose random compliment
-        let randomIndex = Math.floor(Math.random() * compliments.length);
-        let randomCompliment = compliments[randomIndex];
-      
-        res.status(200).send(randomCompliment);
-    }
+    // choose random compliment
+    let randomIndex = Math.floor(Math.random() * compliments.length);
+    let randomCompliment = compliments[randomIndex];
 
-}
+    res.status(200).send(randomCompliment);
+  },
+
+  getFortune: (req, res) => {
+    const fortunes = [
+      "Advice, when most needed, is least heeded",
+      "All the effort you are making will ultimately pay off",
+      "All the troubles you have will pass away very quickly",
+      "All will go well with your new project",
+      "All your hard work will soon pay off",
+    ];
+
+    // choose random fortune
+    let randomIndex = Math.floor(Math.random() * fortunes.length);
+    let randomFortune = fortunes[randomIndex];
+
+    res.status(200).send(randomFortune);
+  },
+};
