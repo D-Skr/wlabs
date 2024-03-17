69d90dd55c2c408d4e0775e9b2414cfedafcb93a
Dmitriy <altusj@gmail.com>
2024-03-16 20:00:16 -0400

    add solution

--- equalizer/server.js
+++ equalizer/server.js
@@ -1,9 +1,39 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+const jwtMiddleware = require("./src/middlewares/jwtMiddleware");
+const authRoutes = require("./src/routes/authRoutes");
+const expenseRoutes = require("./src/routes/expenseRoutes");
+
 const app = express();
-const path = require("path");
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
 
-app.listen(4567, () => {
-  console.log("Server running on http://localhost:4567");
+// Connect to MongoDB Atlas
+mongoose
+  .connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+// Define routes
+app.use("/api/auth", authRoutes);
+app.use("/api/expenses", jwtMiddleware, expenseRoutes);
+//app.use("/api", require("./src/routes"));
+
+// Serve static files
+app.use(express.static("public"));
+
+// Start the server
+const PORT = process.env.SERVER_PORT;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Server error!");
 });
